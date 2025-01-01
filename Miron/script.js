0ab17790603cb4c670be7621371f89d4db5837de
Miron2008 <mironvojcehovskij@gmail.com>
2025-01-01 15:37:37 +0300

    Delete Miron directory

--- Miron/script.js
+++
@@ -1,9 +0,0 @@
-function redirect() {
-    window.location.href="https://getbootstrap.com/"
-}
-function redirect2() {
-    window.location.href="https://flask.palletsprojects.com/en/stable/"
-}
-function redirect3() {
-    window.location.href="https://www.w3schools.com/"
-}
