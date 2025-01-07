cc8815d8b2286eac39aa8633572ecd6d3db8fcd1
Miron2008 <mironvojcehovskij@gmail.com>
2025-01-07 20:25:16 +0300

    Add files via upload

---
+++ Miron1.0/Miron1.0/script.js
@@ -0,0 +1,9 @@
+function redirect() {
+    window.location.href="https://getbootstrap.com/"
+}
+function redirect2() {
+    window.location.href="https://flask.palletsprojects.com/en/stable/"
+}
+function redirect3() {
+    window.location.href="https://www.w3schools.com/"
+}
